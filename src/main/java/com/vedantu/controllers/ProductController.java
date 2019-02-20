55d2fc66af14d85f18c233f3d0937946796813e1
Aswinikumar555 <37668545+Aswinikumar555@users.noreply.github.com>
2019-02-20 17:13:48 +0530

    Update ProductController.java

--- src/main/java/com/vedantu/controllers/ProductController.java
+++ src/main/java/com/vedantu/controllers/ProductController.java
@@ -17,56 +17,55 @@ import com.vedantu.requests.ProductReq;
 @RestController
 @RequestMapping("test1")
 public class ProductController {
-	
 
-    @Autowired
-    private ProductMongoDAO productMongoDAO;
+	@Autowired
+	private ProductMongoDAO productMongoDAO;
+
 //Product APIS	
 	@RequestMapping(value = "/addProduct", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public ProductMongo addParam(@RequestBody ProductReq param) throws Exception {
-	       
-		   ProductMongo e2 = new ProductMongo();
-		 
-	        e2.setName(param.getName());
-	        e2.setQuantity(param.getQuantity());
-	        e2.setWid(param.getWid());
-	        e2.setPrice(param.getPrice());
-	        e2.setProductType(param.getProductType());
-	        //e2.setCartitems(param.getCartitems());
-	        productMongoDAO.create(e2);
+	@ResponseBody
+	public ProductMongo addParam(@RequestBody ProductReq param) throws Exception {
 
-	       return e2;
-	   }
-	
-	//update
-	   @RequestMapping(value="/updateProduct", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public String updateParam(@RequestBody ProductReq param) throws Exception {
-		   
-		   ProductMongo e2 =productMongoDAO.getById(param.getId());
-	        e2.setName(param.getName());
-	        e2.setQuantity(param.getQuantity());
-	        e2.setWid(param.getWid());
-	        e2.setPrice(param.getPrice());
-	       
-	        productMongoDAO.update(e2,null);
+		ProductMongo e2 = new ProductMongo();
 
-	       return "SuccessSuccess";
-		   
-		  }
-	   
-	 //delete
-	   @RequestMapping(value="/deleteProduct", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public String deleteParam(@RequestBody ProductReq param) throws Exception {
-		   
-		   ProductMongo e2 =productMongoDAO.getEntityById(param.getId(), ProductMongo.class);
-		   
-	       
-		   productMongoDAO.delete(e2,null);
+		e2.setName(param.getName());
+		e2.setQuantity(param.getQuantity());
+		e2.setWid(param.getWid());
+		e2.setPrice(param.getPrice());
+		e2.setProductType(param.getProductType());
+		// e2.setCartitems(param.getCartitems());
+		productMongoDAO.create(e2);
 
-	       return "Delete Success";
-	   }
+		return e2;
+	}
+
+	// update
+	@RequestMapping(value = "/updateProduct", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String updateParam(@RequestBody ProductReq param) throws Exception {
+
+		ProductMongo e2 = productMongoDAO.getById(param.getId());
+		e2.setName(param.getName());
+		e2.setQuantity(param.getQuantity());
+		e2.setWid(param.getWid());
+		e2.setPrice(param.getPrice());
+
+		productMongoDAO.update(e2, null);
+
+		return "Update Success";
+
+	}
+
+	// delete
+	@RequestMapping(value = "/deleteProduct", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String deleteParam(@RequestBody ProductReq param) throws Exception {
+
+		ProductMongo e2 = productMongoDAO.getEntityById(param.getId(), ProductMongo.class);
+
+		productMongoDAO.delete(e2, null);
+
+		return "Delete Success";
+	}
 
 }
