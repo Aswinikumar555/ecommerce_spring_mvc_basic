27ee500034b7118732fac1d2042620810848cd92
Aswinikumar555 <37668545+Aswinikumar555@users.noreply.github.com>
2019-02-20 17:14:51 +0530

    Update TestController.java

--- src/main/java/com/vedantu/controllers/TestController.java
+++ src/main/java/com/vedantu/controllers/TestController.java
@@ -25,75 +25,70 @@ import org.springframework.web.servlet.tags.Param;
 @RequestMapping("test")
 public class TestController {
 
-    @Autowired
-    private EmployeeDAO employeeDAO;
+	@Autowired
+	private EmployeeDAO employeeDAO;
 
-    @Autowired
-    private CustomerMongoDAO customerMongoDAO;
-    
+	@Autowired
+	private CustomerMongoDAO customerMongoDAO;
+
 //customer APIS	
-	   @RequestMapping(value = "/addCustomer", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public CustomerMongo addParam(@RequestBody CustomerReq param) throws Exception {
-	       
-		   CustomerMongo e2 = new CustomerMongo();
-		   
-	        e2.setName(param.getName());
-	        e2.setPhno(param.getPhno());
-	        e2.setAddress(param.getAddress());
-	        e2.setAmount(param.getAmount());
-	        customerMongoDAO.create(e2);
+	@RequestMapping(value = "/addCustomer", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CustomerMongo addParam(@RequestBody CustomerReq param) throws Exception {
 
-	       return e2;
-	   }
-	   
-	   
-	   //update
-	   @RequestMapping(value="/updateCustomer", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public String updateParam(@RequestBody CustomerReq param) throws Exception {
-		   
-		   CustomerMongo e2 =customerMongoDAO.getById(param.getId());
-		   
-		   e2.setName(param.getName());
-	        e2.setPhno(param.getPhno());
-	        e2.setAddress(param.getAddress());
-	        e2.setAmount(param.getAmount());
-	       
-	        customerMongoDAO.update(e2,null);
+		CustomerMongo e2 = new CustomerMongo();
 
-	       return "SuccessSuccess";
-		   
-		  }
-	   
-	   //delete
-	   @RequestMapping(value="/deleteCustomer", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public String deleteParam(@RequestBody CustomerReq param) throws Exception {
-		   
-		   CustomerMongo e2 =customerMongoDAO.getEntityById(param.getId(), CustomerMongo.class);
-		   
-	       
-	        customerMongoDAO.delete(e2,null);
+		e2.setName(param.getName());
+		e2.setPhno(param.getPhno());
+		e2.setAddress(param.getAddress());
+		e2.setAmount(param.getAmount());
+		customerMongoDAO.create(e2);
 
-	       return "Delete Success";
-	   }
-    //get
-	   @RequestMapping(value="/getd", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	   @ResponseBody
-	   public String getParam(@RequestBody EmployeeReq param) throws Exception {
-	   	Employee e =employeeDAO.getEntityById(param.getId(), null, Employee.class);
-	       
-	      return "details "+e;
-	   }
-	   
-    
-    
-    
+		return e2;
+	}
 
-    @RequestMapping(value = "get", method = RequestMethod.GET)
-    @ResponseBody
-    public String getParamById() throws Exception {
-        return "RESPONSE";
-    }
+	// update
+	@RequestMapping(value = "/updateCustomer", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String updateParam(@RequestBody CustomerReq param) throws Exception {
+
+		CustomerMongo e2 = customerMongoDAO.getById(param.getId());
+
+		e2.setName(param.getName());
+		e2.setPhno(param.getPhno());
+		e2.setAddress(param.getAddress());
+		e2.setAmount(param.getAmount());
+
+		customerMongoDAO.update(e2, null);
+
+		return "SuccessSuccess";
+
+	}
+
+	// delete
+	@RequestMapping(value = "/deleteCustomer", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String deleteParam(@RequestBody CustomerReq param) throws Exception {
+
+		CustomerMongo e2 = customerMongoDAO.getEntityById(param.getId(), CustomerMongo.class);
+
+		customerMongoDAO.delete(e2, null);
+
+		return "Delete Success";
+	}
+
+	// get
+	@RequestMapping(value = "/getd", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getParam(@RequestBody EmployeeReq param) throws Exception {
+		Employee e = employeeDAO.getEntityById(param.getId(), null, Employee.class);
+
+		return "details " + e;
+	}
+
+	@RequestMapping(value = "get", method = RequestMethod.GET)
+	@ResponseBody
+	public String getParamById() throws Exception {
+		return "RESPONSE";
+	}
 }
